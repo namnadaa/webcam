6a783fb72800b25438862addb82048b80f2d195e
Alexander Nesterov <looss016@gmail.com>
2026-04-22 15:04:40 +0300

    add compare function

---
+++ internal/camera/compare.go
@@ -0,0 +1,64 @@
+package camera
+
+import "fmt"
+
+// CompareInternalExternal compares internal camera with all external cameras.
+func CompareInternalExternal(cameras []Config) {
+	var internal *Config
+	var externals []Config
+
+	for _, cam := range cameras {
+		if cam.IsExternal {
+			externals = append(externals, cam)
+		} else {
+			internal = &cam
+		}
+	}
+
+	if internal == nil {
+		fmt.Println("Internal camera not found")
+		return
+	}
+
+	if len(externals) == 0 {
+		fmt.Println("External cameras not found")
+		return
+	}
+
+	fmt.Println("Camera Comparison")
+	fmt.Println("===============================================================")
+
+	fmt.Printf("%-20s %-15s", "Parameter", "Internal")
+	for i := range externals {
+		fmt.Printf(" %-15s", fmt.Sprintf("External %d", externals[i].Index))
+	}
+	fmt.Println()
+
+	fmt.Println("---------------------------------------------------------------")
+
+	fmt.Printf("%-20s %-15dx%-7d",
+		"Resolution",
+		internal.Width, internal.Height,
+	)
+
+	for _, ext := range externals {
+		fmt.Printf(" %-15dx%-7d", ext.Width, ext.Height)
+	}
+	fmt.Println()
+
+	fmt.Printf("%-20s %-15.2f", "FPS", internal.FPS)
+
+	for _, ext := range externals {
+		fmt.Printf(" %-15.2f", ext.FPS)
+	}
+	fmt.Println()
+
+	fmt.Printf("%-20s %-15s", "Format", internal.Format)
+
+	for _, ext := range externals {
+		fmt.Printf(" %-15s", ext.Format)
+	}
+	fmt.Println()
+
+	fmt.Println("===============================================================")
+}
