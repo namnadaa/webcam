f8bd2cee12d6ac7734e1b4a92e89c45f5a2a64b5
Alexander Nesterov <looss016@gmail.com>
2026-06-09 18:34:48 +0300

    removed unnecessary feature

--- internal/pipeline/sink.go
+++ internal/pipeline/sink.go
@@ -1,12 +1,1 @@
 package pipeline
-
-import (
-	"gocv.io/x/gocv"
-)
-
-// WindowSink — outputs frames to the window (without waitKey).
-func WindowSink(win *gocv.Window) func(Frame) {
-	return func(f Frame) {
-		win.IMShow(f.Img)
-	}
-}
